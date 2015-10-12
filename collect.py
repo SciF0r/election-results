83f71ec3ba2da6db4827a8e858f3dd84d78a9a10
Denis Simonet <denis.simonet@adfinis-sygroup.ch>
2015-10-12 21:19:11 +0200

    Make it work with all lists

--- collect.py
+++ collect.py
@@ -18,7 +18,6 @@ URL = {
     'csv'      : 'pdfs/waehleranteileGemeinde{}.csv',
 }
 COMMUNE_PATTERN = re.compile(r'.*gem=(\d{3}).html')
-LIST_NAME = 'PIRATEN / PIRATES'
 
 STR = {
     'entitled'         : 'Wahlberechtigte / Nombre d\'électeurs',
@@ -31,11 +30,28 @@ STR = {
     'direct_votes'     : 'Kandidatenstimmen /Total des suffrages nominatifs',
     'additional_votes' : 'Zusatzstimmen / Suffrages complémentaires',
     'list_votes'       : 'Parteistimmen / Total des suffrages de parti',
+    'all_votes'        : 'Total',
+    'list'             : 'Liste',
 }
 
-election_results = {}
-votes_all = 0
-votes_pirates = 0
+election_results = {
+    'all_votes': 0,
+    'list_votes': {},
+}
+candidate_results = {}
+
+
+class List(object):
+    """Holds the results for a list"""
+
+    def __init__(self, number, name):
+        self.name = name
+        self.number = number
+        self.direct_votes     = 0
+        self.additional_votes = 0
+        self.list_votes       = 0
+        self.candidates       = []
+
 
 class Commune(object):
     """Holds the results for a commune"""
@@ -43,72 +59,80 @@ class Commune(object):
     def __init__(self, name, path):
         self.name             = name
         self.path             = path
-        self.absolute_votes   = 0
-        self.relative_votes   = 0.0
+        self.total_votes      = 0
         self.entitled         = 0
         self.voted            = 0
         self.empty            = 0
         self.invalid          = 0
         self.valid            = 0
         self.turnout          = 0.0
-        self.direct_votes     = 0
-        self.additional_votes = 0
-        self.candidates       = []
+        self.lists            = []
         self._in_header       = True
-        self._in_party        = False
+        self._current_list    = None
 
     def read_header_row(self, row):
         """Handles a row in header context"""
-        global votes_all
-        global votes_pirates
-        if len(row) == 4 and row[1] == LIST_NAME:
-            absolute_votes = get_csv_int(row[2])
-            relative_votes = get_csv_float(row[3])
-            self.absolute_votes = absolute_votes
-            self.relative_votes = relative_votes
-            votes_pirates = votes_pirates + absolute_votes
-        if len(row) == 2:
-            if row[0] == STR['entitled']:
-                self.entitled = get_csv_int(row[1])
-            if row[0] == STR['voted']:
-                self.voted = get_csv_int(row[1])
-            if row[0] == STR['empty']:
-                self.empty = get_csv_int(row[1])
-            if row[0] == STR['invalid']:
-                self.invalid = get_csv_int(row[1])
-            if row[0] == STR['valid']:
-                valid_votes = get_csv_int(row[1])
-                self.valid = valid_votes
-                votes_all = votes_all + valid_votes
-            if row[0] == STR['turnout']:
-                self.turnout = get_csv_float(row[1])
-                self._in_header = False
+        if row[0] == STR['entitled']:
+            self.entitled = get_csv_int(row[1])
+        elif row[0] == STR['voted']:
+            self.voted = get_csv_int(row[1])
+        elif row[0] == STR['empty']:
+            self.empty = get_csv_int(row[1])
+        elif row[0] == STR['invalid']:
+            self.invalid = get_csv_int(row[1])
+        elif row[0] == STR['valid']:
+            valid_votes = get_csv_int(row[1])
+            self.valid = valid_votes
+        elif row[0] == STR['all_votes']:
+            self.all_votes = get_csv_int(row[2])
+            global election_results
+            election_results['all_votes'] += self.all_votes
+        elif row[0] == STR['turnout']:
+            self.turnout = get_csv_float(row[1])
+            self._in_header = False
 
     def read_party_row(self, row):
         """Handles a row in party context"""
-        if row[0] == STR['direct_votes']:
-            self.direct_votes = get_csv_int(row[1])
+        if row[0] == STR['list']:
+            list_number = get_csv_int(row[1])
+            list_name   = row[2]
+            self._current_list = List(list_number, list_name)
+            self.lists.append(self._current_list)
+        elif row[0] == STR['direct_votes']:
+            self._current_list.direct_votes = get_csv_int(row[1])
         elif row[0] == STR['additional_votes']:
-            self.additional_votes = get_csv_int(row[1])
+            self._current_list.additional_votes = get_csv_int(row[1])
         elif row[0] == STR['list_votes']:
-            self.list_votes = get_csv_int(row[1])
+            list_votes = get_csv_int(row[1])
+            self._current_list.list_votes = list_votes
+            global election_results
+            list_number = self._current_list.number
+            if list_number not in election_results['list_votes']:
+                election_results['list_votes'][list_number] = 0
+            election_results['list_votes'][list_number] += list_votes
         elif len(row) == 9 and row[1] != 'Name' and row[1] != 'Nom':
             try:
-                first_name = row[1].strip()
-                last_name  = row[2].strip()
+                first_name = row[1]
+                last_name  = row[2]
                 votes      = get_csv_int(row[6])
-                self.candidates.append({
-                    'last_name': first_name,
-                    'first_name': last_name,
-                    'votes': votes,
-                })
-                if first_name in election_results:
-                    old_votes = election_results[first_name]
-                    election_results[first_name] = old_votes + votes
-                else:
-                    election_results[first_name] = votes
+                self._add_candidate(first_name, last_name, votes)
             except ValueError:
                 pass
+
+    def _add_candidate(self, first_name, last_name, votes):
+        global candidate_results
+        self._current_list.candidates.append({
+            'first_name': first_name,
+            'last_name': last_name,
+            'votes': votes,
+        })
+        full_name = '{} {}'.format(first_name, last_name)
+        list_number = self._current_list.number
+        if list_number not in candidate_results:
+            candidate_results[list_number] = {}
+        if full_name not in candidate_results[list_number]:
+            candidate_results[list_number][full_name] = 0
+        candidate_results[list_number][full_name] += votes
 
     def fill(self):
         """Fills Commune object with the results"""
@@ -131,13 +155,44 @@ class Commune(object):
             if self._in_header:
                 self.read_header_row(row)
             else:
-                if not self._in_party:
-                    if row[0] == STR['short'] and row[1] == LIST_NAME:
-                        self._in_party = True
-                    continue
                 self.read_party_row(row)
-                if row[0] == 'Liste':
-                    break
+
+
+class Election(object):
+    """Collect election results"""
+
+    def __init__(self):
+        self.communes = []
+
+    def collect(self):
+        """Collect all the temporary results"""
+        print('Collecting...')
+        html = get_html('{}{}'.format(URL['base'], URL['overview']))
+        commune_list = html.findAll('a', {'href': COMMUNE_PATTERN})
+        commune_results = []
+        for commune in commune_list:
+            commune_object = Commune(
+                commune.get_text().strip(),
+                commune.attrs['href'].strip(),
+            )
+            print('.', end='', flush=True)
+            commune_object.fill()
+            self.communes.append(commune_object)
+
+    def candidates_sorted(self, list_number):
+        list_ = candidate_results[list_number]
+        return sorted(
+            list_.items(),
+            key=lambda x: x[1],
+            reverse=True
+        )
+
+    def list_results(self, list_number):
+        list_votes = election_results['list_votes'][list_number]
+        return {
+            'absolute': list_votes,
+            'relative': 100 * list_votes / election_results['all_votes']
+        }
 
 
 def get_html(url):
@@ -146,42 +201,20 @@ def get_html(url):
     return bs4.BeautifulSoup(page, 'html5lib')
 
 
-def get_results():
-    """Returns all the (temporary) results as an object"""
-    print('Getting results...')
-    html = get_html('{}{}'.format(URL['base'], URL['overview']))
-    commune_list = html.findAll('a', {'href': COMMUNE_PATTERN})
-    commune_results = []
-    for commune in commune_list:
-        commune_object = Commune(
-            commune.get_text().strip(),
-            commune.attrs['href'].strip(),
-        )
-        print('.'.format(commune_object.name), end='', flush=True)
-        commune_object.fill()
-        commune_results.append(commune_object)
-    return commune_results
-
-
-def write_results_html(communes):
+def write_results_html(election):
     """Write the results into an html file"""
-    candidates = sorted(
-        election_results.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
-    votes_pirates_relative = 100 * votes_pirates / votes_all
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader('templates')
     )
     template = env.get_template('results.html')
-    template.stream(
-        communes=communes,
-        candidates=candidates,
-        votes_all=votes_all,
-        votes_pirates=votes_pirates,
-        votes_pirates_relative=votes_pirates_relative
-    ).dump('output/results.html')
+    for list_number in election_results['list_votes'].keys():
+        template.stream(
+            communes=election.communes,
+            candidates=election.candidates_sorted(list_number),
+            results=election.list_results(list_number),
+            all_votes=election_results['all_votes'],
+            list_number=list_number
+        ).dump('output/{}.html'.format(list_number))
 
 
 def get_csv_int(str_):
@@ -194,5 +227,6 @@ def get_csv_float(str_):
     return float(str_.strip().replace('\'', '').replace('%', ''))
 
 
-results = get_results()
-write_results_html(results)
+election = Election()
+election.collect()
+write_results_html(election)
